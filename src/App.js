bf8e7022bb07f8e99e90178f11d57e56b5570945
minhthongdev <thonglelele@gmail.com>
2021-01-27 23:27:59 +0700

    all

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Route, BrowserRouter, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import { connect } from "react-redux";
+import { createAction } from "./redux/actions";
+import { SET_TOKEN } from "./redux/actions/type";
+import Detail from "./pages/Detail";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path="/signin" component={Signin} />
+          <Route path="/detail/:courseId" component={Detail} />
+          <Route path="/" component={Home} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+
+  componentDidMount() {
+    const token = localStorage.getItem("t");
+
+    if (token) {
+      this.props.dispatch(createAction(SET_TOKEN, token));
+    }
+  }
 }
-
-export default App;
+export default connect()(App);
